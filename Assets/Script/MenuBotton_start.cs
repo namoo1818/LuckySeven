062a7e4e457a2c9f7a7b87bfe5ab8b70d4112239
이민지 <50236187+namoo1818@users.noreply.github.com>
2019-08-04 22:16:46 +0900

    Add files via upload

---
+++ Assets/Script/MenuBotton_start.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuBotton_start : MonoBehaviour
+{
+    public void ChangeFirstScene()
+    {
+        SceneManager.LoadScene("0_start");
+    }
+
+    public void ChangeSecondScene()
+    {
+        SceneManager.LoadScene("1_character");
+    }
+
+    public void ChangeThirdScene()
+    {
+        SceneManager.LoadScene("2_story");
+    }
+
+    public void ChangeFourthScene()
+    {
+        SceneManager.LoadScene("3_stage");
+    }
+}
